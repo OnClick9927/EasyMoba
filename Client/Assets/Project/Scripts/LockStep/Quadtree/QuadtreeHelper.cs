d1eb607398fdee7dee5a5f31a202195d2f33132c
叶子 <1016345213@qq.com>
2022-10-18 17:01:15 +0800

    添加图形

--- Client/Assets/Project/Scripts/LockStep/Quadtree/QuadtreeHelper.cs
+++ Client/Assets/Project/Scripts/LockStep/Quadtree/QuadtreeHelper.cs
@@ -1,4 +1,5 @@
 ﻿using LMath;
+using System.Drawing;
 
 namespace LCollision2D
 {
@@ -15,15 +16,92 @@ namespace LCollision2D
             node.nodes.Clear();
             node.area = LRect.zero;
         }
-        static bool C2C(CircleShape a, CircleShape b)
+
+        /// <summary>
+        /// 圆圆碰撞
+        /// </summary>
+        /// <param name="circle1">圆a</param>
+        /// <param name="circle2">圆b</param>
+        /// <returns>是否碰撞</returns>
+        static bool Circle2Circle(CircleShape circle1, CircleShape circle2)
         {
-            return a.radius + b.radius > (a.position - b.position).magnitude;
+            return circle1.Radius + circle2.Radius > (circle1.position - circle2.position).magnitude;
         }
+
+        /// <summary>
+        /// 圆点碰撞
+        /// </summary>
+        /// <param name="circle">圆</param>
+        /// <param name="point">点</param>
+        /// <returns>是否碰撞</returns>
+        static bool Circle2Point(CircleShape circle, LVector2 point)
+        {
+            //比较半径和点与圆心的距离
+            return circle.Radius > (circle.position - point).magnitude;
+        }
+
+        /// <summary>
+        /// 圆矩形碰撞
+        /// </summary>
+        /// <param name="circle">圆形</param>
+        /// <param name="rect">矩形</param>
+        /// <returns>是否碰撞</returns>
+        static bool Circle2Rectangle(CircleShape circle, RectangleShape rect)
+        {
+            //旋转圆心（角度为b的角度）得到新的坐标
+            LVector2 newCenter = new LVector2();
+            if (rect.angle != 0)
+            {
+                newCenter.x = Math.Cos(rect.angle) * (circle.position.x - rect.position.x) - Math.Sin(rect.angle) * (circle.position.y - rect.position.y) + rect.position.x;
+                newCenter.y = Math.Cos(rect.angle) * (circle.position.x - rect.position.x) + Math.Cos(rect.angle) * (circle.position.y - rect.position.y) + rect.position.x;
+            }
+            else
+            {
+                newCenter = circle.position;
+            }
+            //判断圆心与矩形的碰撞
+
+            //获取最近的点
+            LVector2 nearPoint = new LVector2();
+            if (newCenter.x < (rect.position.x - rect.Width / 2))
+            {
+                nearPoint.x = rect.position.x - rect.Width / 2;
+
+            }
+            else if (newCenter.x > (rect.position.x + rect.Width / 2))
+            {
+                nearPoint.x = rect.position.x + rect.Width / 2;
+            }
+            else
+            {
+                nearPoint.x = newCenter.x;
+            }
+
+            if (newCenter.y < (rect.position.y - rect.Height / 2))
+            {
+                nearPoint.y = rect.position.y - rect.Height / 2;
+            }
+            else if (newCenter.y > rect.position.y + rect.Height / 2)
+            {
+                nearPoint.x = rect.position.x + rect.Height / 2;
+            }
+            else
+            {
+                nearPoint.y = newCenter.y;
+            }
+
+            var distance = Math.Sqrt(Math.Sqr(nearPoint.x - newCenter.x) + Math.Sqr(nearPoint.y - newCenter.y));
+
+            return distance < circle.Radius;
+        }
+
+
+
         public static bool CouldCollision(Shape a, Shape b)
         {
             if (a is CircleShape && b is CircleShape)
             {
-                return C2C(a as CircleShape, b as CircleShape);
+                return Circle2Circle(a as CircleShape, b as CircleShape);
             }
             return true;
         }
--- Client/Assets/Project/Scripts/LockStep/Quadtree/Shape.cs
+++ Client/Assets/Project/Scripts/LockStep/Quadtree/Shape.cs
@@ -5,14 +5,183 @@ namespace LCollision2D
 {
     public abstract class Shape
     {
+        /// <summary>
+        /// 角度
+        /// </summary>
         public LFloat angle;
+        /// <summary>
+        /// 位置
+        /// </summary>
         public LVector2 position;
+        /// <summary>
+        /// 比例
+        /// </summary>
+        public LFloat scale;
+        /// <summary>
+        /// 最大半径
+        /// </summary>
         public abstract LFloat maxRadius { get; }
     }
+
+    /// <summary>
+    /// 圆
+    /// </summary>
     public class CircleShape : Shape
     {
+        /// <summary>
+        /// 半径
+        /// </summary>
         public LFloat radius;
-        public override LFloat maxRadius => radius;
+        /// <summary>
+        /// 最大半径
+        /// </summary>
+        public override LFloat maxRadius => Radius;
+
+        /// <summary>
+        /// 实际半径
+        /// </summary>
+        public LFloat Radius => radius * scale; 
+    }
+
+    /// <summary>
+    /// 矩形
+    /// </summary>
+    public class RectangleShape : Shape
+    {
+        public LFloat width;
+        public LFloat height;
+
+        public override LFloat maxRadius
+        {
+            get
+            {
+                return Math.Sqrt(Math.Sqr(Width) + Math.Sqr(Height)) / 2;
+            }
+        }
+
+        /// <summary>
+        /// 实际宽
+        /// </summary>
+        public LFloat Width => width * scale;
+        /// <summary>
+        /// 实际高
+        /// </summary>
+        public LFloat Height => height * scale;
+    }
+
+    /// <summary>
+    /// 点的集合
+    /// </summary>
+    public struct Bound
+    {
+        public LVector2[] points;
+    }
+
+    /// <summary>
+    /// 多边形
+    /// </summary>
+    public class PolygonShape : Shape
+    {
+        /// <summary>
+        /// 多边形的点
+        /// </summary>
+        public Bound bound;
+
+        public override LFloat maxRadius
+        {
+            get
+            {
+                LFloat maxRadiusSqr = 0;
+                LFloat tmpRadiusSqr;
+                foreach (var point in Bound.points)
+                {
+                    tmpRadiusSqr = Math.Sqr(point.x) + Math.Sqr(point.y);
+                    maxRadiusSqr = Math.Max(maxRadiusSqr, tmpRadiusSqr);
+                }
+                return Math.Sqrt(maxRadiusSqr);
+            }
+        }
+
+        /// <summary>
+        /// 实际点
+        /// </summary>
+        public Bound Bound;
+    }
+
+    /// <summary>
+    /// 扇形
+    /// </summary>
+    public class SectorShape : Shape
+    {
+        /// <summary>
+        /// 半径
+        /// </summary>
+        public LFloat radius;
+
+        /// <summary>
+        /// 扇形角
+        /// </summary>
+        public LFloat sectorAngle;
+
+        /// <summary>
+        /// 最大半径
+        /// </summary>
+        public override LFloat maxRadius => Radius;
+
+
+        public LFloat Radius => radius * scale;
+    }
+
+
+
+    /// <summary>
+    /// 射线
+    /// </summary>
+    public struct Ray
+    {
+        /// <summary>
+        /// 位置
+        /// </summary>
+        public LVector2 position;
+        /// <summary>
+        /// 方向
+        /// </summary>
+        public LVector2 direction;
+
+        int layer;
+    }
+
+    /// <summary>
+    /// 射线击中的shape
+    /// </summary>
+    public struct RayHit
+    {
+        public bool success;
+        public Shape[] shapes;
+    }
+
+    /// <summary>
+    /// 直线
+    /// </summary>
+    public struct Line
+    {
+        /// <summary>
+        /// 直线上一点
+        /// </summary>
+        public LVector2 point;
+        /// <summary>
+        /// 方向
+        /// </summary>
+        public LVector2 direction;
+    }
+
+    /// <summary>
+    /// 线段
+    /// </summary>
+    public struct Segment
+    {
+        public LVector2 point1;
+        public LVector2 point2;
     }
 }
 
